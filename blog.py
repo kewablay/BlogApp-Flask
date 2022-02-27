98d47f8a913dc57af15af20b63ded513eaecaf48
kewablay <kojokewajunior123@gmail.com>
2022-02-27 22:00:43 +0000

    added detail page routing

--- blog.py
+++ blog.py
@@ -1,7 +1,7 @@
 from email.policy import default
 from unicodedata import category
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 
@@ -39,8 +39,38 @@ def home():
     categories = Category.query.all()
     return render_template('index.html', posts=all_post, categories=categories)
 
+
+@app.route('/<int:id>', methods=['GET', 'POST'])
+def postDetail(id):
+    post = Post.query.get(id)
+    return render_template("detail.html", post=post)
+
+
 @app.route('/new-post',methods=['GET', 'POST'])
 def newPost():
+    if request.method == "POST":
+        category_id = int(request.form['categoriesOption'])
+        title = request.form['title']
+        content = request.form['content']
+        author = request.form['author']
+        image = request.form['image']
+        
+
+        new_post = Post(category_id=category_id, image_file=image, title=title, content=content,  author=author )
+    
+        db.session.add(new_post)
+        db.session.commit()
+        
+        print("post created sucessfully...")
+        
+        # if request.form['image']:
+        # else: 
+        #     new_post = Post(category=category, title=title, content=content,  author=author )
+            
+        #     db.session.add(new_post)
+        #     db.session.commit()
+        return redirect('/')
+    
     return render_template('new-post.html')
 
 if __name__ == "__main__":
