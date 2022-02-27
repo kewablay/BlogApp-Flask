cc0b886744b14d6cf8894d7dd567dc450b92f252
kewablay <kojokewajunior123@gmail.com>
2022-02-27 02:15:04 +0000

    added db models

--- blog.py
+++ blog.py
@@ -1,6 +1,24 @@
+from unicodedata import category
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+class Category(db.Model):
+    category_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    post_id = db.Column(db.Integer, ForeignKey())
+
 
 @app.route('/')
 def blog():
